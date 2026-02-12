ace20438486e8ce9b9e47298e21eb673a6d8ab46
Maribel <maribelhs2006@gmail.com>
2026-02-12 12:15:29 +0100

    12/02/2026 nivel 1

---
+++ Sistemas-Operativos-2/README.md
@@ -0,0 +1,2 @@
+# Sistemas-Operativos-2
+Proyecto Sistemas Operativos 2 - Ingenieria Informatica UIB
---
+++ bloques.c
@@ -0,0 +1,23 @@
+#include "bloques.h"
+
+static int descriptor = 0;
+
+//Funcion para montar el dipositivo virtual
+int bmount(const char *camino){
+    //Programar
+}
+
+//Funcion para desmontar el dispotivo viertual
+int bumount(){
+    //Programar
+}
+
+//Funcion que sirve para escribir en un bloque
+int bwrite(unsigned int nbloque, const void *buf){
+    //Programar
+}
+
+//Funcion para leer un bloque
+int bread(unsigned int nbloque, void *buf){
+    //Programar
+}
