879000712210d26b50a5b3a5646678887f0d3b6e
Maribel <maribelhs2006@gmail.com>
2026-02-14 17:04:17 +0100

    nivel 1 V1
    Aquí esta mi parte del nivel 1

--- bloques.c
+++ bloques.c
@@ -4,17 +4,34 @@ static int descriptor = 0;
 
 //Funcion para montar el dipositivo virtual
 int bmount(const char *camino){
-    //Programar
+    descriptor = open(camino, O_RDWR | O_CREAT, 0666); //Abrimos el fichero
+    if(descriptor == FALLO){ 
+        perror(RED "Error"); 
+        return FALLO;
+    }
+    return descriptor;
 }
 
 //Funcion para desmontar el dispotivo viertual
 int bumount(){
     //Programar
+    return FALLO; //esto solo es para poder programar lo otro
 }
 
 //Funcion que sirve para escribir en un bloque
 int bwrite(unsigned int nbloque, const void *buf){
-    //Programar
+    off_t desplazamiento = nbloque * BLOCKSIZE; //calculamos el desplazamiento para saber donde empezar a escribir
+    off_t pos = lseek(descriptor, desplazamiento , SEEK_SET); //movemos el puntero del fichero en el offset correto
+    if(pos == FALLO){
+        perror(RED "Error"); 
+        return FALLO;
+    }
+    size_t bytes = write(descriptor, buf, BLOCKSIZE); //volcamos el contenido del buffer en el dispositivo virtual
+    if(bytes == FALLO){
+        perror(RED "Error"); 
+        return FALLO;
+    }
+    return bytes;
 }
 
 //Funcion para leer un bloque
--- mi_mkfs.c
+++ mi_mkfs.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include "bloques.c"
 
 int main(int argc, char **argv){
-
+    unsigned char buffer[BLOCKSIZE];
+    bmount(argv[1]); //creamos el dispositivo virtual
+    for(int i = 0; i < atoi(argv[2]); i++){ //bucle for para escribir los bloques
+        memset(buffer, 0 , BLOCKSIZE);
+        bwrite(i, buffer);
+    }
+    bunmount();  //desmontamos el dispostivo virtual
 }
