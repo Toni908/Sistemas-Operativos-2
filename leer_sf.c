408ffe6320a9f7838e865ff6e91290c50381e5b2
Antonio Garcia Font <tlovesc908@gmail.com>
2026-02-23 13:34:28 +0100

    leer_sf

---
+++ leer_sf.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "ficheros_basico.h"
+
+int main(int argc, char **argv){
+
+    if(argc != 2){
+        perror(RED "Error"); 
+        return FALLO;
+    }
+
+    // Montar dispositivo
+    if(bmount(argv[1]) == FALLO){
+        perror(RED "Error"); 
+        return FALLO;
+    }
+
+    struct superbloque SB;
+
+    // Leer superbloque
+    if(bread(posSB, &SB) == FALLO){
+        perror(RED "Error"); 
+        bumount();
+        return FALLO;
+    }
+
+    printf("DATOS DEL SUPERBLOQUE\n");
+    printf("posPrimerBloqueMB = %u\n", SB.posPrimerBloqueMB);
+    printf("posUltimoBloqueMB = %u\n", SB.posUltimoBloqueMB);
+    printf("posPrimerBloqueAI = %u\n", SB.posPrimerBloqueAI);
+    printf("posUltimoBloqueAI = %u\n", SB.posUltimoBloqueAI);
+    printf("posPrimerBloqueDatos = %u\n", SB.posPrimerBloqueDatos);
+    printf("posUltimoBloqueDatos = %u\n", SB.posUltimoBloqueDatos);
+    printf("posInodoRaiz = %u\n", SB.posInodoRaiz);
+    printf("posPrimerInodoLibre = %u\n", SB.posPrimerInodoLibre);
+    printf("cantBloquesLibres = %u\n", SB.cantBloquesLibres);
+    printf("cantInodosLibres = %u\n", SB.cantInodosLibres);
+    printf("totBloques = %u\n", SB.totBloques);
+    printf("totInodos = %u\n", SB.totInodos);
+
+    printf("\nsizeof struct superbloque: %lu\n", sizeof(struct superbloque));
+    printf("sizeof struct inodo: %lu\n", sizeof(struct inodo));
+
+    printf("\nRECORRIDO LISTA ENLAZADA DE INODOS LIBRES\n");
+
+    unsigned int posInodo = SB.posPrimerInodoLibre;
+    struct inodo inodo;
+
+    while(posInodo != UINT_MAX){
+
+        if(leer_inodo(posInodo, &inodo) == FALLO){
+            perror("Error leyendo inodo");
+            bumount();
+            return FALLO;
+        }
+
+        printf("%u ", posInodo);
+
+        posInodo = inodo.punterosDirectos[0];
+    }
+
+    printf("-1\n");
+
+    bumount();
+    return EXITO;
+}
