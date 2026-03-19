66a7757f781e1598acb89453eb12bcfbecd71e88
Toni908 <tlovesc908@gmail.com>
2026-03-19 11:21:11 +0100

    fix errores

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -207,7 +207,7 @@ int reservar_bloque(){
             posbit++;
         }
         int nbloque = (nbloqueMB * BLOCKSIZE  +  posbyteB) * 8 + posbit;
-        escribir_bit(nbloque, 1);        
+        if(escribir_bit(nbloque, 1) == FALLO) return FALLO;        
         SB.cantBloquesLibres --;
 
         if(bwrite(posSB, &SB) == FALLO) return FALLO; // guardar SB
@@ -221,9 +221,9 @@ int reservar_bloque(){
 
 int liberar_bloque(unsigned int nbloque){
     struct superbloque SB;
-    if (bread(posSB, &SB) == FALLO) return FALLO;
+    if(bread(posSB, &SB) == FALLO) return FALLO;
 
-    escribir_bit(nbloque, 0);
+    if(escribir_bit(nbloque, 0) == FALLO) return FALLO;
     SB.cantBloquesLibres ++;
 
     if(bwrite(posSB, &SB) == FALLO) return FALLO;  // guardar SB
@@ -250,7 +250,7 @@ int escribir_inodo(unsigned int ninodo, struct inodo *inodo){
 int leer_inodo(unsigned int ninodo, struct inodo *inodo){
     struct inodo inodos[BLOCKSIZE/INODOSIZE];
     struct superbloque SB;
-    if (bread(posSB, &SB) == FALLO) return FALLO;
+    if(bread(posSB, &SB) == FALLO) return FALLO;
 
     int nbloqueAI = ninodo * INODOSIZE / BLOCKSIZE;
     int nbloqueabs = nbloqueAI + SB.posPrimerBloqueAI;
@@ -262,7 +262,7 @@ int leer_inodo(unsigned int ninodo, struct inodo *inodo){
 
 int reservar_inodo(unsigned char tipo, unsigned char permisos){
     struct superbloque SB;
-    if (bread(posSB, &SB) == FALLO) return FALLO;
+    if(bread(posSB, &SB) == FALLO) return FALLO;
     
     if(SB.cantInodosLibres != 0){
         struct inodo inodo;
@@ -364,6 +364,7 @@ int traducir_bloque_inodo(unsigned int ninodo, unsigned int nblogico, unsigned c
     if (leer_inodo(ninodo, &inodo) == FALLO) return FALLO;
 
     nRangoBL = obtener_nRangoBL(&inodo, nblogico, &ptr);
+    if(nRangoBL == FALLO) return FALLO;
     nivel_punteros = nRangoBL;
     // Punetros Directos
     if (nRangoBL == 0){
@@ -372,6 +373,7 @@ int traducir_bloque_inodo(unsigned int ninodo, unsigned int nblogico, unsigned c
                 return FALLO;
             }
             ptr = reservar_bloque();
+            if(ptr == FALLO) return FALLO;
             inodo.punterosDirectos[nblogico] = ptr;
             inodo.numBloquesOcupados++;
             inodo.ctime = time(NULL); 
@@ -387,6 +389,7 @@ int traducir_bloque_inodo(unsigned int ninodo, unsigned int nblogico, unsigned c
                     return FALLO;
                 }
                 ptr = reservar_bloque(); // bloque de punteros
+                if(ptr == FALLO) return FALLO;
                 inodo.numBloquesOcupados++;
                 inodo.ctime = time(NULL);
                 salvar_inodo = 1;
@@ -405,9 +408,10 @@ int traducir_bloque_inodo(unsigned int ninodo, unsigned int nblogico, unsigned c
                 }
                 memset(buffer, 0, BLOCKSIZE);
             }else{
-                bread(ptr, buffer);
+                if(bread(ptr, buffer) == FALLO) return FALLO;
             }
             indice = obtener_indice(nblogico, nivel_punteros);
+            if(indice == FALLO) return FALLO;
             ptr_ant = ptr;
             ptr = buffer[indice];
 
@@ -419,12 +423,13 @@ int traducir_bloque_inodo(unsigned int ninodo, unsigned int nblogico, unsigned c
                 return FALLO;
             }
             ptr = reservar_bloque();
+            if(ptr == FALLO) return FALLO;
             inodo.numBloquesOcupados++;
             inodo.ctime = time(NULL);
             salvar_inodo = 1;
 
             buffer[indice] = ptr;
-            bwrite(ptr_ant, buffer);
+            if(bwrite(ptr_ant, buffer) == FALLO) return FALLO;
             
             #if DEBUG && NIVEL4
                 printf(GRAY "[traducir_bloque_inodo()→ punteros_nivel1 [%d] = %u (reservado BF %u para BL %u)]\n" RESET, indice, ptr, ptr, nblogico);
@@ -435,7 +440,7 @@ int traducir_bloque_inodo(unsigned int ninodo, unsigned int nblogico, unsigned c
         printf("\n");
     #endif
     if (salvar_inodo){
-        escribir_inodo(ninodo, &inodo);
+        if(escribir_inodo(ninodo, &inodo) == FALLO) return FALLO;
     }
 
     return ptr;
@@ -466,6 +471,7 @@ int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offse
     // Caso 1: todo en el mismo bloque
     if (primerBL == ultimoBL){
         bf = traducir_bloque_inodo(ninodo, primerBL, 1);
+        if(bf == FALLO) return FALLO;
         bread(bf, buf_bloque);
         memcpy(buf_bloque + desp1, buf_original, nbytes); // destino origen bytes
         bwrite(bf, buf_bloque);
@@ -474,6 +480,7 @@ int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offse
     else { // Caso 2:
         // Primer Bloque (1024 - desp1 B)
         bf = traducir_bloque_inodo(ninodo, primerBL, 1);
+        if(bf == FALLO) return FALLO;
         bread(bf, buf_bloque);
         memcpy(buf_bloque + desp1, buf_original, BLOCKSIZE - desp1);
         bwrite(bf, buf_bloque);
@@ -482,12 +489,14 @@ int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offse
         // Bloques Intermedios (1024B)
         for (unsigned int i = primerBL + 1; i < ultimoBL; i++){
             bf = traducir_bloque_inodo(ninodo, i, 1);
+            if(bf == FALLO) return FALLO;
             bwrite(bf, buf_original + bytes_escritos);
             bytes_escritos += BLOCKSIZE;
         }
 
         // Ultimo Bloque (despl2 +1 B, +1 porque se escribe el 0)
         bf = traducir_bloque_inodo(ninodo, ultimoBL, 1);
+        if(bf == FALLO) return FALLO;
         bread(bf, buf_bloque);
         memcpy(buf_bloque, buf_original + bytes_escritos, desp2 + 1);
         bwrite(bf, buf_bloque);
@@ -539,41 +548,37 @@ int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsi
     // Caso 1
     if (primerBL == ultimoBL){
         bf = traducir_bloque_inodo(ninodo, primerBL, 0);
-        if (bf != FALLO){
-            bread(bf, buf_bloque);
-            memcpy(buf_original, buf_bloque + desp1, nbytes);  // destino origen bytes
-        }
+        if(bf == FALLO) return FALLO;
+        bread(bf, buf_bloque);
+        memcpy(buf_original, buf_bloque + desp1, nbytes);  // destino origen bytes
         bytes_leidos = nbytes;
     } 
     else { // Caso 2
         // Primer Bloque
         bf = traducir_bloque_inodo(ninodo, primerBL, 0);
-        if (bf != FALLO){
-            bread(bf, buf_bloque);
-            memcpy(buf_original, buf_bloque + desp1, BLOCKSIZE - desp1);
-        }
+        if(bf == FALLO) return FALLO;
+        bread(bf, buf_bloque);
+        memcpy(buf_original, buf_bloque + desp1, BLOCKSIZE - desp1);
         bytes_leidos += BLOCKSIZE - desp1;
 
         // Bloque Intermedio
         for (unsigned int i = primerBL + 1; i < ultimoBL; i++){
             bf = traducir_bloque_inodo(ninodo, i, 0);
-            if (bf != FALLO){
-                bread(bf, buf_bloque);
-                memcpy(buf_original + bytes_leidos, buf_bloque, BLOCKSIZE);
-            }
+            if(bf == FALLO) return FALLO;
+            bread(bf, buf_bloque);
+            memcpy(buf_original + bytes_leidos, buf_bloque, BLOCKSIZE);
             bytes_leidos += BLOCKSIZE;
         }
         // Ultimo Bloque
         bf = traducir_bloque_inodo(ninodo, ultimoBL, 0);
-        if (bf != FALLO){
-            bread(bf, buf_bloque);
-            memcpy(buf_original + bytes_leidos, buf_bloque, desp2 + 1);
-        }
+        if(bf == FALLO) return FALLO;
+        bread(bf, buf_bloque);
+        memcpy(buf_original + bytes_leidos, buf_bloque, desp2 + 1);
         bytes_leidos += desp2 + 1; // incluimos el 0
     }
 
     inodo.atime = time(NULL);
-    escribir_inodo(ninodo, &inodo);
+    if(escribir_inodo(ninodo, &inodo) == FALLO) return FALLO;
     return bytes_leidos;
 }
 
