d3ad71f3f1c135d8ca2b507bb8e3a6d749e84cfa
Maribel <maribelhs2006@gmail.com>
2026-04-28 18:28:52 +0200

    el mi_cat

---
+++ mi_cat.c
@@ -0,0 +1,33 @@
+#include "directorios.h"
+
+#define TAMBUFFER 1500
+
+int main(int argc, char **argv){
+    
+    // Comprobamos la sintaxis 
+    if(argc != 2){
+        fprintf(stderr, RED "Sintaxis: ./mi_cat <disco> </ruta_fichero>\n" RESET);
+        return FALLO;
+    }
+
+    //Comprobamos si es un fichero (NO debe terminar en '/')
+    if(argv[3][strlen(argv[3]) - 1] == '/'){
+        fprintf(stderr, RED "Error: la ruta no debe terminar en '/' para crear un fichero\n" RESET);
+        return FALLO;
+    }
+
+    if(bmount(argv[1]) == FALLO){ // disco virtual
+        return FALLO;
+    }
+
+    char buffer[TAMBUFFER];
+    int leidos = 0;
+    unsigned int offset = 0;
+
+    // Leer bloque a bloque
+    memset(buffer, 0, TAMBUFFER);
+    leidos = mi_read(argv[2], buffer, offset, TAMBUFFER);
+
+    bumount();
+    return EXITO;
+}
