de356c5dccde6e921124bfbbfe9e58dc61914005
Antonio Garcia Font <tlovesc908@gmail.com>
2026-05-22 18:16:42 +0200

    Update semaforo_mutex_posix.c

--- semaforo_mutex_posix.c
+++ semaforo_mutex_posix.c
@@ -1,8 +1,8 @@
-//Antonio García Font y Maria Isabel Herrero Soteras  
-/* semaforo_mutex_posix.c */
 #include "semaforo_mutex_posix.h"
 
+
 /* Ejemplo de creación e inicialización de semáforos POSIX para MUTEX con "semáforos con nombre" (named) */
+
 
 sem_t *initSem() {
    /* name debe ser un nombre de caracteres ascii que comienze con "/", p.e. "/mimutex" */
@@ -11,14 +11,25 @@ sem_t *initSem() {
 
    sem = sem_open(SEM_NAME, O_CREAT, S_IRWXU, SEM_INIT_VALUE);
    if (sem == SEM_FAILED) {
+       perror("Error sem_open()");
        return NULL;
    }
    return sem;
 }
 
 
-void deleteSem() {
+/* void deleteSem() {
    sem_unlink(SEM_NAME);
+   // borra el semáforo del sistema y da problemas cuando
+   // los hijos en la simulación ejecutan bumount()
+} */
+
+
+void deleteSem(sem_t *sem) {
+   //printf("Closing sem in address: %p\n", (void *)sem);
+   if (sem_close(sem) == -1) {
+       perror("Error deleteSem()");
+   }
 }
 
 
@@ -30,5 +41,7 @@ void signalSem(sem_t *sem) {
 void waitSem(sem_t *sem) {
    sem_wait(sem);
 }
+
+
 
 
